1c4000977a13e66cab4dd22a55f6ddc96246559d
Prayuj Tuli <prayujtuli@hotmail.com>
2024-08-27 01:14:11 -0400

    added blue bar, and dynamic initial scrolling based on current time

--- src/lib/components/CalendarWeek.svelte
+++ src/lib/components/CalendarWeek.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { onMount, onDestroy } from 'svelte';
 
   let dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
   let hours: string[] = [];
@@ -7,13 +7,19 @@
   let currentDay = '';
   let currentTime = '';
 
-  let scrollTopElement: HTMLElement;
-  let refs: { [day: string]: { [hour: string]: HTMLElement } } = {};
-
-  const scrollToStart = () => {
-    if (scrollTopElement)
-      scrollTopElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  let morningElement: HTMLElement;
+  let refs: { [day: string]: { [hour: string]: HTMLElement } } = {
+    SUN: {},
+    MON: {},
+    TUE: {},
+    WED: {},
+    THU: {},
+    FRI: {},
+    SAT: {}
   };
+
+  let barYPosition = -1;
+  let barWidth = -1;
 
   onMount(() => {
       getWeekDates();
@@ -25,7 +31,35 @@
           tempHours.push(`${i} PM`);
       }
       hours = tempHours;
+
+      currentTime = new Date().toLocaleTimeString();
+
+      window.addEventListener('resize', updateCurrentTimeBar);
+
+      onDestroy(() => {
+          window.removeEventListener('resize', updateCurrentTimeBar);
+      });
+
+      setInterval(() => {
+          currentTime = new Date().toLocaleTimeString();
+      }, 60000);
   });
+
+  const scrollToStart = () => {
+      if (!currentDay) return;
+      const hour = getCurrentHour();
+      if (hour < 8) {
+          refs[currentDay]['12 AM'].scrollIntoView({ behavior: 'smooth', block: 'start' });
+      } else if (hour >= 20) {
+          refs[currentDay]['8 PM'].scrollIntoView({ behavior: 'smooth', block: 'start' });
+      } else if (morningElement) {
+          morningElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+  };
+
+  const getCurrentHour = () => new Date().getHours();
+  const getHourString = () => currentTime.replace(currentTime.substring(currentTime.indexOf(':'), currentTime.indexOf(' ')), '');
+  const getMinuteFraction = () => parseInt(currentTime.substring(currentTime.indexOf(':') + 1, currentTime.indexOf(':') + 3)) / 60;
 
   const getWeekDates = (inputDate = new Date()) => {
     const today = new Date(inputDate);
@@ -50,12 +84,27 @@
     }
 
     weekDates = [...tempWeekDates];
-    currentTime = today.toLocaleTimeString();
-    console.log(currentTime)
   }
 
-  $: scrollTopElement, scrollToStart();
+  const updateCurrentTimeBar = () => {
+      if (refs[currentDay] && refs[currentDay][getHourString()]) {
+          const rect = refs[currentDay][getHourString()].getBoundingClientRect();
+          barYPosition = (rect.y + getMinuteFraction() * rect.height);
+          const leftEdge = refs['SUN']['12 AM'].getBoundingClientRect().left;
+          const rightEdge = refs['SAT']['12 AM'].getBoundingClientRect().right;
+          barWidth = rightEdge - leftEdge + 25;
+      }
+  }
+
+  $: morningElement, scrollToStart();
+
+  $: refs, currentTime, updateCurrentTimeBar();
 </script>
+
+<div
+    class="ml-[22rem] fixed rounded-full h-0.5 w-[calc(100%-22.5rem)] bg-blue-500 text-white overflow-auto"
+    style="top: {barYPosition}px; width: {barWidth}px;">
+</div>
 
 <div class="ml-72 mr-4 flex flex-col h-screen">
     <div class="ml-20 mb-2 grid grid-cols-7">
@@ -69,11 +118,11 @@
         {/each}
     </div>
 
-    <div class="flex-1 overflow-auto">
+    <div class="flex-1 overflow-auto" on:scroll={updateCurrentTimeBar}>
         {#each hours as hour}
             <div class="flex">
                 {#if hour === '8 AM'}
-                  <div class="text-xs text-gray-600 -mt-1 mr-2 ml-2 w-16 text-center" bind:this={scrollTopElement}>
+                  <div class="text-xs text-gray-600 -mt-1 mr-2 ml-2 w-16 text-center" bind:this={morningElement}>
                       {hour}
                   </div>
                 {:else}
@@ -82,9 +131,10 @@
                   </div>
                 {/if}
 
-                {#each dayNames as _}
+                {#each dayNames as dayName}
                     <div
                         class="hour-card flex-1 p-2 border-t border-l border-gray-200 bg-white hover:bg-gray-100 transition-colors cursor-pointer"
+                        bind:this={refs[dayName][hour]}
                     >
                     </div>
                 {/each}
